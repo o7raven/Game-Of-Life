2b3bafd8d1e001ca5434bd843002f152be53ccd1
Raven <yakubiq@proton.me>
2023-08-31 00:43:20 +0200

    A cross-platform build for the application

---
+++ build/CMakeFiles/GameOfLife.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/raven/Coding/Game-Of-Life/src/include/Grid/Grid.cpp" "CMakeFiles/GameOfLife.dir/src/include/Grid/Grid.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/src/include/Grid/Grid.cpp.o.d"
+  "/home/raven/Coding/Game-Of-Life/src/include/Log/Log.cpp" "CMakeFiles/GameOfLife.dir/src/include/Log/Log.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/src/include/Log/Log.cpp.o.d"
+  "/home/raven/Coding/Game-Of-Life/src/include/Render/Render.cpp" "CMakeFiles/GameOfLife.dir/src/include/Render/Render.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/src/include/Render/Render.cpp.o.d"
+  "/home/raven/Coding/Game-Of-Life/src/include/Simulation/Simulation.cpp" "CMakeFiles/GameOfLife.dir/src/include/Simulation/Simulation.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/src/include/Simulation/Simulation.cpp.o.d"
+  "/home/raven/Coding/Game-Of-Life/src/main.cpp" "CMakeFiles/GameOfLife.dir/src/main.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
