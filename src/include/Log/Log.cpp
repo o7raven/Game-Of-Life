5558af28aaa9db731a014a25af2b35addccf223f
yakubiq raven <yakubiq@proton.me>
2023-08-30 19:51:13 +0200

    A simple log function

---
+++ src/include/Log/Log.cpp
@@ -0,0 +1,4 @@
+#include "Log.hpp"
+void log(const std::string& message){
+    std::cout << "[LOG] " << message << std::endl;
+}
---
+++ src/include/Log/Log.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+#include <string>
+void log(const std::string& message);
